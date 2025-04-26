69edcbf8cd29a76f6a4b017466bac7a3b08eb0a4
Soumya Purani <soumyapurani@gmail.com>
2025-04-26 17:48:50 -0300

    Made navbar responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import React from 'react'
 import './App.css'
 import Navbar from './UI/Navbar'
+import MobileMenu from './UI/MobileMenu'
 
 function App() {
-
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   return (
-    <Navbar />
+    <div className="text-3xl font-bold underline">
+      <Navbar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
+      <MobileMenu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
+    </div>
   )
 }
 
--- src/UI/Navbar.jsx
+++ src/UI/Navbar.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Logo from './Logo'
+import { MdMenu } from "react-icons/md";
 
-function Navbar() {
+function Navbar({isMenuOpen, setIsMenuOpen}) {
+  useEffect(() => {
+    document.body.style.overflow = isMenuOpen ? 'hidden' : '';
+  }, [isMenuOpen]);
   return (
     <nav className="fixed top-o w-full z-40 bg-#1C0D30 shadow-lg backdrop-blur-lg px-1 border-white">
         <div className="max-w-5xl mx-auto px-4"> 
             <div className="flex justify-between items-center font-mono h-16">
                 <Logo />
+                {!isMenuOpen && <div onClick={() => setIsMenuOpen(prev => !prev)} className="text-2xl absolute right-7 z-40m md:hidden cursor-pointer font-mono">
+                  <MdMenu />
+                </div>}
                 <div className="flex md:flex items-center space-x-8">
                   <div className="hidden md:flex items-center space-x-8">
                     <a href="#home" className="text-gray-300 text-lg hover:text-xl hover:text-white transition-all">Home</a>
