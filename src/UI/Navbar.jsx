1687b12bdfe4a8cc866217e43fe5048361e5b761
Soumya Purani <soumyapurani@gmail.com>
2025-04-26 15:42:57 -0300

    Added navbar

--- src/UI/Navbar.jsx
+++ src/UI/Navbar.jsx
@@ -7,6 +7,14 @@ function Navbar() {
         <div className="max-w-5xl mx-auto px-4"> 
             <div className="flex justify-between items-center font-mono h-16">
                 <Logo />
+                <div className="flex md:flex items-center space-x-8">
+                  <div className="hidden md:flex items-center space-x-8">
+                    <a href="#home" className="text-gray-300 text-lg hover:text-xl hover:text-white transition-all">Home</a>
+                    <a href="#about" className="text-gray-300 text-lg hover:text-xl hover:text-white transition-all">About</a>
+                    <a href="#projects" className="text-gray-300 text-lg hover:text-xl hover:text-white transition-all">Projects</a>
+                    <a href="#contact" className="text-gray-300 text-lg hover:text-xl hover:text-white transition-all">Contact</a>
+                  </div>
+                </div>
             </div>
         </div>
     </nav>
