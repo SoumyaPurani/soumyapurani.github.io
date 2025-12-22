78dead6bdb11725a4729d8511b47c1f9fb5244e0
Soumya Purani <66162598+SoumyaPurani@users.noreply.github.com>
2025-12-22 16:22:10 -0400

    minor fix for hamburger menu

--- src/UI/MobileMenu.jsx
+++ src/UI/MobileMenu.jsx
@@ -2,7 +2,7 @@ import { RxCross2 } from "react-icons/rx";
 
 function MobileMenu({isMenuOpen, setIsMenuOpen}) {
   return (
-    <div className={`fixed w-full top-0 left-0 bg-[rgba(10,10,10,0.95)] z-40 flex flex-col items-center justify-center h-screen transition-all duration-300 gap-15 ease-in-out ${isMenuOpen ? "h-screen opacity-100 pointer-events-auto" : "h-0 opacity-0 pointer-events-none"}`}>
+    <div className={`fixed w-full top-0 left-0 bg-[rgba(10,10,10,0.95)] z-60 flex flex-col items-center justify-center h-screen transition-all duration-300 gap-15 ease-in-out ${isMenuOpen ? "h-screen opacity-100 pointer-events-auto" : "h-0 opacity-0 pointer-events-none"}`}>
         <button onClick={() => setIsMenuOpen(false)} 
         className="absolute top-5 right-7 text-2xl focus:outline-none cursor-pointer text-white hover:text-[var(--primary-color)] transition-colors" 
         aria-label="Close Menu">
